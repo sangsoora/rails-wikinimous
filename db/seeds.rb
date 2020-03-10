4caefeba0a078bc920041f9a6c6971ede6e325da
Sang Soo Ra <nadang82@gmail.com>
2020-03-10 15:48:27 +0100

    finished wiki

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 10 fakeposts...'
+10.times do
+  article = Article.new(
+    title: Faker::Lorem.word,
+    content: Faker::Lorem.paragraph
+  )
+  article.save!
+end
